4b93168fe95cefc5e7fccc29324ed0b109f64208
bijayaacharya135 <internbijaya@gmail.com>
2025-08-19 08:29:58 +0530

    test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -3,27 +3,27 @@
 #include "vitals.h"
 
 TEST(Vitals, TemperatureConditions) {
-    EXPECT_EQ(mapToCondition(94.0f, tempBoundary), HYPO);
-    EXPECT_EQ(mapToCondition(95.5f, tempBoundary), NEAR_HYPO);
+    EXPECT_EQ(mapToCondition(94.0f, tempBoundary), CRITICAL_LOW);
+    EXPECT_EQ(mapToCondition(95.5f, tempBoundary), WARNING_LOW);
     EXPECT_EQ(mapToCondition(98.0f, tempBoundary), NORMAL);
-    EXPECT_EQ(mapToCondition(101.0f, tempBoundary), NEAR_HYPER);
-    EXPECT_EQ(mapToCondition(103.0f, tempBoundary), HYPER);
+    EXPECT_EQ(mapToCondition(101.0f, tempBoundary), WARNING_HIGH);
+    EXPECT_EQ(mapToCondition(103.0f, tempBoundary), CRITICAL_HIGH);
 }
 
 TEST(Vitals, PulseConditions) {
-    EXPECT_EQ(mapToCondition(59.0f, pulseBoundary), HYPO);
-    EXPECT_EQ(mapToCondition(61.0f, pulseBoundary), NEAR_HYPO);
+    EXPECT_EQ(mapToCondition(59.0f, pulseBoundary), CRITICAL_LOW);
+    EXPECT_EQ(mapToCondition(61.0f, pulseBoundary), WARNING_LOW);
     EXPECT_EQ(mapToCondition(70.0f, pulseBoundary), NORMAL);
-    EXPECT_EQ(mapToCondition(99.0f, pulseBoundary), NEAR_HYPER);
-    EXPECT_EQ(mapToCondition(101.0f, pulseBoundary), HYPER);
+    EXPECT_EQ(mapToCondition(99.5f, pulseBoundary), WARNING_HIGH);
+    EXPECT_EQ(mapToCondition(101.0f, pulseBoundary), CRITICAL_HIGH);
 }
 
 TEST(Vitals, Spo2Conditions) {
-    EXPECT_EQ(mapToCondition(89.0f, spo2Boundary), HYPO);
-    EXPECT_EQ(mapToCondition(91.0f, spo2Boundary), NEAR_HYPO);
+    EXPECT_EQ(mapToCondition(89.0f, spo2Boundary), CRITICAL_LOW);
+    EXPECT_EQ(mapToCondition(91.0f, spo2Boundary), WARNING_LOW);
     EXPECT_EQ(mapToCondition(98.0f, spo2Boundary), NORMAL);
-    EXPECT_EQ(mapToCondition(149.0f, spo2Boundary), NEAR_HYPER);
-    EXPECT_EQ(mapToCondition(151.0f, spo2Boundary), HYPER);
+    EXPECT_EQ(mapToCondition(149.0f, spo2Boundary), WARNING_HIGH);
+    EXPECT_EQ(mapToCondition(151.0f, spo2Boundary), CRITICAL_HIGH);
 }
 
 TEST(Vitals, OverallStatus) {
@@ -36,4 +36,3 @@ TEST(Vitals, OverallStatus) {
     VitalsResult critical = evaluateVitals(94.0f, 70.0f, 98.0f);
     EXPECT_FALSE(overallVitalsOk(critical.temp, critical.pulse, critical.spo2));
 }
-
