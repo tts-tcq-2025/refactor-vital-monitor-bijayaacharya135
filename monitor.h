98729efd75fe50922552e12034d0af6c39a2e8fb
bijayaacharya135 <internbijaya@gmail.com>
2025-09-23 00:38:43 +0530

    monitor.h

--- monitor.h
+++ monitor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "vitals.h"
 
+// Boundaries for each vital sign
 extern const VitalBoundary tempBoundary;
 extern const VitalBoundary pulseBoundary;
 extern const VitalBoundary spo2Boundary;
@@ -11,5 +12,5 @@ struct VitalsResult {
     VitalCondition spo2;
 };
 
+// Pure function for evaluating all vitals
 VitalsResult evaluateVitals(float temperature, float pulse, float spo2);
-
