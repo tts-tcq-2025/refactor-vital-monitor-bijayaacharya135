673caa64ac94e9145bd23e966906d5a695924c51
bijayaacharya135 <internbijaya@gmail.com>
2025-09-23 14:36:53 +0530

    monitor.h

--- monitor.h
+++ monitor.h
@@ -6,6 +6,9 @@ extern const VitalBoundary tempBoundary;
 extern const VitalBoundary pulseBoundary;
 extern const VitalBoundary spo2Boundary;
 
+// Declare supportedLanguages for global access
+extern const std::vector<LanguageCode> supportedLanguages;
+
 struct VitalsResult {
     VitalCondition temp;
     VitalCondition pulse;
@@ -15,4 +18,5 @@ struct VitalsResult {
 // Pure function for evaluating all vitals
 // Added: Accept unit for temperature
 VitalsResult evaluateVitals(float temperature, TempUnit tempUnit, float pulse, float spo2);
+
 
